423a8b39c61c0660f868675ca8c52bc8a22a42d9
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2023-10-27 08:40:52 +0000

    Clean up unused scripts

--- databases/fbm_sum_gen.py
+++
@@ -1,9 +0,0 @@
-from databases.fbm_gen import gen as fbm
-
-def gen(n = 200, hurst1 = 0.5, lambd1 = 1, hurst2 = 0.5, lambd2 = 1):
-
-    fbm1 = fbm(n = n, hurst = hurst1, lambd = lambd1)
-    fbm2 = fbm(n = n, hurst = hurst2, lambd = lambd2)
-
-    return fbm1 + fbm2
-
