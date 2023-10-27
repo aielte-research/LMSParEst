3d9933a2d032c342e5360d761a3d8499d9cfdf23
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2023-10-27 00:35:53 +0000

    cleaning up in root

--- main_trainer.py
+++ main_trainer.py
@@ -17,8 +17,6 @@ import neptune
 
 from tqdm import trange as trange
 
-from calc_distr import CalcDistr
-
 #------------------------------------------------------------------------------
 #                          ABSTRACT LEVEL
 #------------------------------------------------------------------------------
@@ -499,6 +497,34 @@ def import_metric(metric_names):
                                                  in zip(mdl_list, class_names)]
 
     return {k: v for k, v in zip(metric_names, metrics)}
+
+class CalcDistr():
+    def __init__(self, real_inferred, local_radius = 0.0025, lower_bound = 0, 
+                 upper_bound = 1, uniform_chance = 0.05):
+        self.local_radius = local_radius
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.uniform_chance = uniform_chance
+        self.real = real_inferred[0]
+        self.inferred = real_inferred[1]
+        self.losses = [(r - i) ** 2 for r, i in zip(self.real, self.inferred)]
+        self.max_loss = max(self.losses)
+
+    def __call__(self):
+
+        if random.uniform(0, 1) < self.uniform_chance:
+            gen_val = random.uniform(self.lower_bound, self.upper_bound)
+        else:
+            gen_accepted = False
+            while not gen_accepted:
+                r_idx = random.randint(0, len(self.real) - 1)
+                if self.losses[r_idx] >= random.uniform(0, self.max_loss):
+                    gen_accepted = True
+                    gen_val = random.uniform(
+                        max(0, self.real[r_idx] - self.local_radius),
+                        min(1, self.real[r_idx] + self.local_radius))
+
+        return gen_val
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
