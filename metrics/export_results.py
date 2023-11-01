3537e5593db50a861674b6314b255ded9cdabf5b
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2023-11-01 20:33:18 +0000

    floating point precision in exports

--- metrics/export_results.py
+++ metrics/export_results.py
@@ -6,6 +6,7 @@ class Export_results():
     def __init__(self, params, neptune_experiment):
         self.fpath = params.get('fpath')
         self.export_targets = params.get('export_targets',False)
+        self.prec = params.get('precision',17)
         self.neptune_experiment=neptune_experiment
 
     def __call__(self, data):
@@ -16,14 +17,14 @@ class Export_results():
             with my_open(self.fpath, "w") as f:
                 if self.export_targets:
                     f.write(f"result,target,diff,SE\n")
-                    f.write("\n".join([f"{y},{x},{y-x},{(y-x)**2}" for x,y in zip(targets,results)]))
+                    f.write("\n".join([f"{y:.{self.prec}f},{x:.{self.prec}f},{y-x:.{self.prec}f},{(y-x)**2:.{self.prec}f}" for x,y in zip(targets,results)]))
                 else:
                     f.write("\n".join([str(y) for y in results]))
         elif extension == 'tsv':        
             with my_open(self.fpath, "w") as f:
                 if self.export_targets:
                     f.write(f"result\ttarget\tdiff\tSE\n")
-                    f.write("\n".join([f"{y}\t{x}\t{y-x}\t{(y-x)**2}" for x,y in zip(targets,results)]))
+                    f.write("\n".join([f"{y:.{self.prec}f}\t{x:.{self.prec}f}\t{y-x:.{self.prec}f}\t{(y-x)**2:.{self.prec}f}" for x,y in zip(targets,results)]))
                 else:
                     f.write("\n".join([str(y) for y in results]))
         elif extension == 'json':
@@ -33,8 +34,8 @@ class Export_results():
                     "targets":targets,
                     "diff":[y-x for x,y in zip(targets,results)],
                     "SE":[(y-x)**2 for x,y in zip(targets,results)]
-                }
-                f.write(json.dumps(res, indent=4))
+                }                
+                f.write(json.dumps(json.loads(json.dumps(res), parse_float=lambda x: round(float(x), self.prec)), indent=4))
         else:
             raise ValueError(f"Config extension unrecognized: '.{extension}'. Must be '.csv', '.tsv' or '.json'!")
         
