39010ae4a5a061317577595e621815f41048f2ca
31b41a59l26u53 <3.1b41a59l26u53@gmail.com>
2023-11-02 11:28:58 +0000

    More options for the inference script

--- README.md
+++ README.md
@@ -92,7 +92,7 @@ You can specify the save destination in the `train_params/checkpoint_save_path` 
 
 ## Inference
 
-You can also evaluate on your own sequences in a `.csv`,
+Using `inference.py` we can estimate the parameters of our own sequences from a `.csv`,
 ```tsv
 0.14667,0.0,0.23876,0.22432,...,-0.73709
 0.36946,0.0,0.07471,0.07089,...,-2.16548
@@ -117,8 +117,37 @@ or `.json`
 ```
 format.
 
+Run
 ```sh
-   CUDA_VISIBLE_DEVICES=0 python inference.py -f inference_input_data/tst.csv
+python inference.py -h
+```
+to display the options for the script.
+
+```sh
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SERIESTYPE, --seriestype SERIESTYPE
+                        Type of the predicted time series. Options: 'fBm' (Hurst), 'fOU' (Hurst) and 'ARFIMA' (d). (default: fBm)
+  -m MODELTYPE, --modeltype MODELTYPE
+                        Type of the prediction model. Options: 'LSTM' and 'conv1D'. (default: LSTM)
+  -w WEIGHTSFILE, --weightsfile WEIGHTSFILE
+                        File path for the trained model weights. If desired to change the default which comes from the model and sequence selection. (default: None)
+  -b BATCHSIZE, --batchsize BATCHSIZE
+                        Inference batch size. (default: 32)
+  -c CONFIGFILE, --configfile CONFIGFILE
+                        File path for configfile '.yaml'. Only if desired to overwrite the default which comes from the model and sequence selection. (default: None)
+
+required arguments:
+  -i INPUTFILE, --inputfile INPUTFILE
+                        File path for the input '.csv', '.tsv' or '.json' file. (default: None)
+  -o OUTPUTFILE, --outputfile OUTPUTFILE
+                        File path for the output '.csv', '.tsv' or '.json' file. (default: None)
+```
+
+Example usage:
+
+```sh
+   CUDA_VISIBLE_DEVICES=0 python inference.py -i inference_input_data/tst.csv -o inference_output_data/tst.tsv
 ```
 
 See the `data_params` field of the `fBm_Hurst_LSTM_eval_from_csv.yaml` config filr for more information on input csv format.
--- inference.py
+++ inference.py
@@ -5,22 +5,62 @@ from main_trainer import SessionTrainer
 #if run as a script
 if __name__ == "__main__":
     #parse command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--configfile', required=False, default=None, type=str, help="File path for configfile '.yaml'")
-    parser.add_argument('-f', '--inputfile', required=True, default=None, type=str, help="File path for the input '.csv', '.tsv' or '.json' file")
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    required = parser.add_argument_group('required arguments')
+    required.add_argument('-i', '--inputfile', required=True, default=None, type=str, help="File path for the input '.csv', '.tsv' or '.json' file.")
+    required.add_argument('-o', '--outputfile', required=True, default=None, type=str, help="File path for the output '.csv', '.tsv' or '.json' file.")
+    parser.add_argument('-s', '--seriestype', required=False, default="fBm", type=str, help="Type of the predicted time series. Options: 'fBm' (Hurst), 'fOU' (Hurst) and 'ARFIMA' (d).")
+    parser.add_argument('-m', '--modeltype', required=False, default="lstm", type=str, help="Type of the prediction model. Options: 'LSTM' and 'conv1D'.")
+    parser.add_argument('-w', '--weightsfile', required=False, default=None, type=str, help="File path for the trained model weights. If desired to change the default which comes from the model and sequence selection.")
+    parser.add_argument('-b', '--batchsize', required=False, default=32, type=int, help="Inference batch size.")
+    parser.add_argument('-c', '--configfile', required=False, default=None, type=str, help="File path for configfile '.yaml'. Only if desired to overwrite the default which comes from the model and sequence selection.")
     args = parser.parse_args()
 
     if args.configfile is None:
-        config_fpath="configs/eval/fBm/fBm_Hurst_LSTM_eval_from_file.yaml"
+        seriestype = args.seriestype.lower()
+        modeltype = args.modeltype.lower()
+        if seriestype=="fbm":
+            if modeltype=="lstm":
+                config_fpath="configs/eval/fBm/fBm_Hurst_LSTM_eval_from_file.yaml"
+            elif modeltype=="conv1d":
+                raise ValueError(f"Option '{modeltype}' not implemented for '{seriestype}' yet!")
+            else:
+                raise ValueError(f"Model type '{args.modeltype}' not recognized! Available options: 'LSTM' and 'conv1D'")  
+        elif seriestype=="fou":
+            if modeltype=="lstm":
+                raise ValueError(f"Option '{modeltype}' not implemented for '{seriestype}' yet!")
+            elif modeltype=="conv1d":
+                raise ValueError(f"Option '{modeltype}' not implemented for '{seriestype}' yet!")
+            else:
+                raise ValueError(f"Model type '{args.modeltype}' not recognized! Available options: 'LSTM' and 'conv1D'")
+        elif seriestype=="arfima":
+            if modeltype=="lstm":
+                raise ValueError(f"Option '{modeltype}' not implemented for '{seriestype}' yet!")
+            elif modeltype=="conv1d":
+                raise ValueError(f"Option '{modeltype}' not implemented for '{seriestype}' yet!")
+            else:
+                raise ValueError(f"Model type '{args.modeltype}' not recognized! Available options: 'LSTM' and 'conv1D'")
+        else:
+            raise ValueError(f"Series type '{args.seriestype}' not recognized! Available options: 'fBm', 'fOU' and 'ARFIMA'")        
     else:
         config_fpath=args.configfile
 
     orig_cfg = cfg_parser.read_file(config_fpath)
     if type(orig_cfg) is dict:
-        orig_cfg["data_params"]["fpath"]=args.inputfile 
+        orig_cfg["data_params"]["fpath"]=args.inputfile
+        orig_cfg["train_params"]["metrics"]["session"]["export_results"]["metric_params"]["fpath"]=args.outputfile
+        orig_cfg["data_params"]["train_batch_size"]=args.batchsize
+        orig_cfg["data_params"]["val_batch_size"]=args.batchsize
+        if args.weightsfile is not None:
+            orig_cfg["model_checkpoint_path"]=args.weightsfile
     elif type(orig_cfg) is list:
-        for exp in orig_cfg:
-            exp["data_params"]["fpath"]=args.inputfile
+        for experiment in orig_cfg:
+            experiment["data_params"]["fpath"]=args.inputfile
+            experiment["train_params"]["metrics"]["session"]["export_results"]["metric_params"]["fpath"]=args.outputfile
+            experiment["data_params"]["train_batch_size"]=args.batchsize
+            experiment["data_params"]["val_batch_size"]=args.batchsize
+            if args.weightsfile is not None:
+                experiment["model_checkpoint_path"]=args.weightsfile
     else:
         raise ValueError(f"Unrecognized config format!")
 
